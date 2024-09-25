bdc43bf7f28cd086d3055e4a7a86100701fa6bd4
Reyhane <reyhane_rbee75@yahoo.com>
2024-09-25 22:13:36 +0330

    Update views.py

--- core/views.py
+++ core/views.py
@@ -1,8 +1,1 @@
-from rest_framework import status, generics
-from .models import ActiveLogicalBase
-from .serializers import ActiveSerializer
 
-
-class ActiveDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = ActiveLogicalBase.objects.all()
-    serializer_class = ActiveSerializer
