80092cdf14aa9e39d44d3d2826a814385b89d14d
Reyhane <reyhane_rbee75@yahoo.com>
2024-09-25 22:14:19 +0330

    Update views.py

--- favorite/views.py
+++ favorite/views.py
@@ -1,9 +1,9 @@
-from rest_framework import status, generics
-from .models import Favorite
-from .serializers import FavoriteSerializer
+# from rest_framework import status, generics
+# from .models import Favorite
+# from .serializers import FavoriteSerializer
 
 
-class FavoriteDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Favorite.objects.all()
-    serializer_class = FavoriteSerializer
+# class FavoriteDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Favorite.objects.all()
+#     serializer_class = FavoriteSerializer
 
