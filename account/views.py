cc8da6594d97aebe5ce3b417168b403d254506aa
Reyhane <reyhane_rbee75@yahoo.com>
2024-09-25 22:12:31 +0330

    Update views.py

--- account/views.py
+++ account/views.py
@@ -1,9 +1,1 @@
-from rest_framework import status, generics
-from .models import Human
-from .serializers import HumanSerializer
-
-
-class HumanDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Human.objects.all()
-    serializer_class = HumanSerializer
 
