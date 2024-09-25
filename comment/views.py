563a2bbd8ea78d39f60b9a8f6e1528e120d99bf8
Reyhane <reyhane_rbee75@yahoo.com>
2024-09-25 22:12:59 +0330

    Update views.py

--- comment/views.py
+++ comment/views.py
@@ -1,8 +1,1 @@
-from rest_framework import status, generics
-from .models import Comments
-from .serializers import CommentsSerializer
 
-
-class CommentsDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Comments.objects.all()
-    serializer_class = CommentsSerializer
