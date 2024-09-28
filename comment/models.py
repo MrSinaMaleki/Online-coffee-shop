2a0cba216833cf894c911f2a9208a8fa35dba997
reyhane <reyhane_rbee75@yahoo.com>
2024-09-29 00:03:23 +0330

    updating managers & models

--- comment/models.py
+++ comment/models.py
@@ -1,15 +1,19 @@
 from django.db import models
-from account.models import DeleteLogicalBase, Human
+from account.models import LogicalMixin, Human
 from product.models import Product
 from django.core.exceptions import ValidationError
+from core.managers import ActiveNotDeletedBaseManager
 
 
-class AcceptedManager(models.Manager):
-    def get_queryset(self):
+class CommentManager(ActiveNotDeletedBaseManager):
+    def accepted(self):
         return super().get_queryset().filter(is_accepted=True)
 
+    def not_accepted(self):
+        return super().get_queryset().filter(is_accepted=False)
 
-class Comments(DeleteLogicalBase):
+
+class Comments(LogicalMixin):
     text = models.TextField(max_length=500)
     user = models.ForeignKey(Human, on_delete=models.SET_NULL, null=True, related_name='comments')
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='comments')
@@ -17,13 +21,14 @@ class Comments(DeleteLogicalBase):
     is_accepted = models.BooleanField(default=False)
     reply_comments = models.ForeignKey('Comments', on_delete=models.CASCADE, null=True,
                                        related_name='child', blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    objects = models.Manager()
-    accepted = AcceptedManager()
+
+    objects = CommentManager()
 
     def clean(self):
         if not (0 <= self.score <= 5):
             raise ValidationError('Score must be between 0 and 5')
+        if self.reply_comments and self.reply_comments == self:
+            raise ValidationError('A comment cannot reply to itself.')
 
     def save(self, *args, **kwargs):
         self.clean()
--- core/models.py
+++ core/models.py
@@ -1,15 +1,27 @@
 from django.db import models
 
 
-class ActiveLogicalBase(models.Model):
+class LogicalMixin(models.Model):
     is_active = models.BooleanField(default=True)
-
-    class Meta:
-        abstract = True
-
-
-class DeleteLogicalBase(models.Model):
     is_delete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
+
+    def activate(self):
+        self.is_active = True
+        self.save(update_fields=['is_active'])
+
+    def deactivate(self):
+        self.is_active = False
+        self.save(update_fields=['is_active'])
+
+    def make_delete(self):
+        self.is_delete = True
+        self.save(update_fields=['is_delete'])
+
+    def make_undelete(self):
+        self.is_delete = False
+        self.save(update_fields=['is_delete'])
