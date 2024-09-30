234a1e3c51bd8e2ea008a3f76864ac5d7daa99fd
MrSinaMaleki <sina67631@gmail.com>
2024-09-30 20:08:20 +0330

    Django admin pannel removal of account user tables due to security issues

--- account/admin.py
+++ account/admin.py
@@ -1,16 +1,16 @@
-from django.contrib import admin
-from .models import Human
-
-
-class HumanAdmin(admin.ModelAdmin):
-    list_display = ('username', 'phone', 'gender', 'profile_image', 'get_age')
-    list_filter = ('phone',)
-    search_fields = ('username', 'phone')
-    search_help_text = 'please enter phone number or username!'
-
-    def get_age(self, obj):
-        return obj.age
-    get_age.short_description = 'Age'
-
-
-admin.site.register(Human, HumanAdmin)
+# from django.contrib import admin
+# from .models import Human
+#
+#
+# class HumanAdmin(admin.ModelAdmin):
+#     list_display = ('username', 'phone', 'gender', 'profile_image', 'get_age')
+#     list_filter = ('phone',)
+#     search_fields = ('username', 'phone')
+#     search_help_text = 'please enter phone number or username!'
+#
+#     def get_age(self, obj):
+#         return obj.age
+#     get_age.short_description = 'Age'
+#
+#
+# admin.site.register(Human, HumanAdmin)
