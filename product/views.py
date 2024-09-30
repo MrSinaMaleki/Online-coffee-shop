8fbdb70a24cfce5c91fb0af4e3f5e0495fbc71f7
reyhane <reyhane_rbee75@yahoo.com>
2024-09-30 22:31:41 +0330

    timeline + pathes for contex processor

--- product/views.py
+++ product/views.py
@@ -1,6 +1,7 @@
 from pprint import pprint
 
 from django.shortcuts import render
+from django.views.generic import View, DetailView, TemplateView
 from rest_framework import status
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 from rest_framework.response import Response
@@ -10,17 +11,41 @@ from product.models import Product, Category
 from product.serializers import ProductSerializer, ProductDetailSerializer
 
 
-def index(request, pk):
-    return render(request, 'product/detailProduct.html', {'pk': pk})
+class ProductDetailView(TemplateView):
+    template_name = 'product/detailProduct.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pk'] = self.kwargs['pk']
+        return context
 
 
-# Create your views here.
 class ProductDetailAPIView(RetrieveAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductDetailSerializer
 
 
-# # todo
+# class ProductDetailView(DetailView):
+#     model = Product
+#     template_name = 'product/detailProduct.html'
+
+# def get_context_data(self, **kwargs):
+#     context = super().get_context_data(**kwargs)
+#     context['pk'] = self.object.pk
+#     if 'product' in context:
+#         del context['product']
+#     return context
+
+
+class CategoryView(TemplateView):
+    template_name = 'product/all_products_category.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['pk'] = self.kwargs['pk']
+        return context
+
+
 class ProductListView(APIView):
     def get(self, request, pk=None):
         if pk is not None:
@@ -29,22 +54,30 @@ class ProductListView(APIView):
                 categories = category.get_descendants(include_self=True)
                 products = Product.objects.filter(category__in=categories)
                 serializer = ProductSerializer(products, many=True)
-                serializer.context['request']=request
+                serializer.context['request'] = request
                 return Response(serializer.data, status=status.HTTP_200_OK)
             return Response(status=status.HTTP_404_NOT_FOUND)
         else:
             products = Product.objects.all().order_by('category')
             serializer = ProductSerializer(products, many=True)
-            serializer.context['request']=request
+            serializer.context['request'] = request
             return Response(serializer.data, status=status.HTTP_200_OK)
 
-    # def get_queryset(self):
-    #     time = self.request.query_params.get('time', None)
-    #     if time:
-    #         return Product.available.filter(
-    #             Q(is_coffee_shop=True) | Q(timeline=time)
-    #         )
-    #
-    #     return Product.available.filter(
-    #         Q(is_coffee_shop=True) | Q(timeline__isnull=False)
-    #     )
+
+class ProductCoffeeShopListView(ListAPIView):
+    queryset = Product.objects.coffeeshop()
+    serializer_class = ProductSerializer
+
+
+class ProductRestaurantListView(ListAPIView):
+    serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        time = self.request.query_params.get('time', None)
+        print('time', time)
+        if time:
+            queryset = Product.objects.filter(timeline=time)
+            print('queryset', queryset)
+            return queryset
+
+
