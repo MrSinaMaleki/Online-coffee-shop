ce96c9d0ca3a0bc3d931b7d361952abb2e448f61
Parisa <parisa.prof92@gmail.com>
2024-10-06 19:06:58 +0330

    hi

--- order/views.py
+++ order/views.py
@@ -1,4 +1,3 @@
-
 from rest_framework import status, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,7 +11,6 @@ from .serializers import OrderItemSerializer, OrderSerializer
 # class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
 #     queryset = Order.objects.all()
 #     serializer_class = OrderSerializer
-
 
 
 # class OrderItemList(APIView):
@@ -43,14 +41,14 @@ class OrderItemList(APIView):
                 serializer = OrderSerializer(orders_is_not_complete, many=True)
                 return Response(serializer.data, status=status.HTTP_200_OK)
         return Response({'message : is not super user'}, status=status.HTTP_403_FORBIDDEN)
+
     def post(self, request):
         print(request.data)
         if request.user.is_superuser:
-            order=Order.objects.filter(pk=request.data['orderId']).first()
+            order = Order.objects.filter(pk=request.data['orderId']).first()
             if order is not None:
                 order.is_completed = True
                 order.save()
-                return Response( status=status.HTTP_200_OK)
-            return Response( status=status.HTTP_400_BAD_REQUEST)
+                return Response(status=status.HTTP_200_OK)
+            return Response(status=status.HTTP_400_BAD_REQUEST)
         return Response({'message : is not super user'}, status=status.HTTP_403_FORBIDDEN)
-
