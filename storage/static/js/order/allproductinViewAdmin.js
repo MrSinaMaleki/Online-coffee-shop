8d4362c1d0de944ce55df1847de236580ff7b2c0
hamidreza <hamedreza1992@gmail.com>
2024-10-06 20:49:43 +0330

    show is coffee shop in admin panel

--- storage/static/js/order/allproductinViewAdmin.js
+++ storage/static/js/order/allproductinViewAdmin.js
@@ -151,3 +151,56 @@ function FuTimeLine(timeLine) {
         });
     });
 }
+
+function coffeeShop(){
+        fetch(`http://localhost:8001/product/api/list/coffeeshop`, {
+            method: 'GET',
+            headers: {
+                'X-Requested-With': 'XMLHttpRequest',
+                'X-CSRFToken': '{{ csrf_token }}'
+            }
+        }).then(async request => {
+        if (!request.ok) {
+            throw new Error('Network response was not ok ' + request.statusText); // Throw an error if the response is not ok
+        }
+        let orderRows2 = '';
+        const pro = await request.json();
+        console.log(pro);
+        tbody_items2.innerHTML = "";
+        for (let i in pro) {
+            orderRows2 += `<tr>  
+                        <th></th>  
+                        <td>  
+                            <div class="flex items-center gap-3">  
+                                <div class="avatar">  
+                                    <div class="mask mask-squircle h-12 w-12">  
+                                        <img src="${pro[i].images.length > 0 ?
+                pro[i].images[0].image :
+                'https://via.placeholder.com/300'}"  
+                                        alt="${pro[i].title} Image" />  
+                                    </div>  
+                                </div>  
+                                <div>  
+                                    <a href="http://localhost:8001/product/${pro[i].id}"><div class="font-bold">${pro[i].title}</div>  </a>
+                                    <div class="text-sm opacity-50">Quantity : ${pro[i].quantity}</div>  
+                                </div>  
+                            </div>  
+                        </td>  
+                        <td>  
+                            ${pro[i].category.title}  
+                            <br />  
+                            <span class="badge badge-ghost badge-sm">${pro[i].description}</span>  
+                        </td>  
+                        <td>${pro[i].timeline}</td>  
+                    </tr>`;
+        }
+        tbody_items2.innerHTML = orderRows2;
+    }).catch(error => {
+        Swal.fire({
+            title: 'Error!',
+            text: 'Error fetching products by timeline: ' + error.message,
+            icon: 'error',
+            confirmButtonText: 'Okay'
+        });
+    });
+}
