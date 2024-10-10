eb86b72d7378887f67725d89325cb56c579eb940
MrSinaMaleki <sina67631@gmail.com>
2024-10-10 16:25:14 +0330

    add ingredients bugs fixed(Arian)

--- storage/static/js/Admin_Panel_Add_Product/AddIngredients.js
+++ storage/static/js/Admin_Panel_Add_Product/AddIngredients.js
@@ -1,57 +1,64 @@
- function fetchIngredients() {
-            const parentSelect = document.getElementById('product');
-            fetch('http://localhost:8001/product/api/add/product/')
-                .then(response => response.json())
-                .then(data => {
-                    console.log(data)
-                    parentSelect.innerHTML = '<option value="">-----</option>';
-                    data.forEach(product => {
-                        const option = document.createElement('option');
-                        option.value = product.id;
-                        option.textContent = product.title;
-                        parentSelect.appendChild(option);
-                    });
-                })
-                .catch(error => {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Error',
-                        text: 'Failed to fetch products.'
-                    });
-                });
-        }
+function fetchIngredients() {
+    const parentSelect = document.getElementById('products_drop');
+    fetch('http://localhost:8001/product/api/add/product/')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            parentSelect.innerHTML = '<option value="">-----</option>';
+            data.forEach(product => {
+                const option = document.createElement('option');
+                option.value = product.id;
+                option.textContent = product.title;
+                parentSelect.appendChild(option);
+            });
+        })
+        .catch(error => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Failed to fetch products.!!!'
+            });
+            console.log('error:', error)
+        });
+}
 
-        document.getElementById('ingredientsForm').addEventListener('submit', function (event) {
-            event.preventDefault();
+document.getElementById('ingredientsForm').addEventListener('submit', function (event) {
+    event.preventDefault();
 
-            const formData = {
-                title: document.getElementById('title').value,
-                products: document.getElementById('product').value || null
-            };
 
-            fetch('http://localhost:8001/product/api/add/ingredients/', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRFToken': csrfTokens
-                },
-                body: JSON.stringify(formData)
-            })
-            .then(response => response.json())
-            .then(data => {
-                Swal.fire({
-                    icon: 'success',
-                    title: 'Success',
-                    text: 'Ingredients added successfully!'
-                });
-                document.getElementById('ingredientsForm').reset();
-                add_ingredients_form.classList.add('hidden');
-            })
-            .catch(error => {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Error',
-                    text: 'Failed to add ingredients.'
-                });
+    let options = document.getElementById('products_drop').selectedOptions;
+    let values = Array.from(options).map(({value}) => value);
+    // console.log('values',values);
+
+    const formData = {
+        title: document.getElementById('title_inp').value,
+        products: values
+    };
+    console.log("formdata: ", formData)
+
+    fetch('http://localhost:8001/product/api/add/ingredients/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrfTokens
+        },
+        body: JSON.stringify(formData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            Swal.fire({
+                icon: 'success',
+                title: 'Success',
+                text: 'Ingredients added successfully!'
+            });
+            document.getElementById('ingredientsForm').reset();
+            add_ingredients_form.classList.add('hidden');
+        })
+        .catch(error => {
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Failed to add ingredients.'
             });
         });
+});
