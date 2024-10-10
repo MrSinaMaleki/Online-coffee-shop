9963baea02557bf212078e53f02b1dfe1ceb4745
MrSinaMaleki <sina67631@gmail.com>
2024-10-11 01:58:58 +0330

    dot-env setup + requirements setup

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+#Imports for dotenv
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(+8=tv)dwx4#x62m96@upos_tl8rl2j8go0-hchhjeday-ub)6'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -147,4 +153,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'onlinecoffeeshopnoreply@gmail.com'
-EMAIL_HOST_PASSWORD = 'scryerxqgoascecx'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
